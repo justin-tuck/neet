bce61366b49faeaa7766257355337ee6df964241
justin-tuck <p.justin.tuck@gmail.com>
2022-10-01 09:51:44 -0400

    added two pointer problem

--- js/TwoPointers/maxArea.js
+++ js/TwoPointers/maxArea.js
@@ -4,14 +4,34 @@ import { UnitTests, Test } from "../Utils/Test.js";
  * @param {number[]} height
  * @return {number}
  */
-var maxArea = function (height) {};
+var maxArea = function (height) {
+  // have a left and right pointer and distance
+  let [l, r, d] = [0, height.length - 1, height.length - 1];
+  //keeping track of the max
+  let max = 0;
+
+  function calcAreaSetMax(h) {
+    let temp = h * d;
+    if (temp > max) max = temp;
+  }
+  // walk through each giving up the lowest wall
+  while (l < r) {
+    if (height[l] < height[r]) {
+      calcAreaSetMax(height[l]);
+      l++;
+    } else {
+      calcAreaSetMax(height[r]);
+      r--;
+    }
+    d--;
+  }
+
+  return max;
+};
 
 let testsToRun = [
-  new Test({ expected: 0, args: [[0, 1]] }),
-  new Test({ expected: 1, args: [[1, 0, 1]] }),
-  new Test({ expected: 1, args: [[1, 0, 2, 1]] }),
-  new Test({ expected: 1, args: [[3, 2, 1, 2, 1]] }),
-  new Test({ expected: 6, args: [[0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]] }),
+  new Test({ expected: 49, args: [[1, 8, 6, 2, 5, 4, 8, 3, 7]] }),
+  new Test({ expected: 1, args: [[1, 1]] }),
 ];
-const test = new UnitTests(trap, testsToRun);
+const test = new UnitTests(maxArea, testsToRun);
 test.run();
